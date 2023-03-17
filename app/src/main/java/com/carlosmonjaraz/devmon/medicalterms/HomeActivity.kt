2275ff23faf4bb4a71888f21746af38ce631fdd7
Carlos Monjaraz <18161148@itoaxaca.edu.mx>
2023-03-16 20:37:44 -0600

    added search

--- app/src/main/java/com/carlosmonjaraz/devmon/medicalterms/HomeActivity.kt
+++ app/src/main/java/com/carlosmonjaraz/devmon/medicalterms/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.carlosmonjaraz.devmon.medicalterms
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.carlosmonjaraz.devmon.medicalterms.databinding.ActivityHomeBinding
 
@@ -9,16 +11,46 @@ class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityHomeBinding
 
+    private var medicalTermsList : MutableList<TermsModel> = Provider.listMedicalTerms.toMutableList()
+    private lateinit var adapter : TermsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initListener()
         initRecyclerView()
     }
 
+    private fun initListener() {
+        binding.svTerm.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?) = false
+
+            override fun onQueryTextChange(newText: String?) : Boolean {
+                searchByTerm(newText.orEmpty())
+                return false
+            }
+        })
+    }
+
     private fun initRecyclerView() {
+        adapter = TermsAdapter(medicalTermsList)
+
         binding.rvMedicalTerms.layoutManager = LinearLayoutManager(this)
-        binding.rvMedicalTerms.adapter = TermsAdapter(Provider.listMedicalTerms)
+        binding.rvMedicalTerms.adapter = adapter
+    }
+
+    private fun searchByTerm(term: String) {
+        val listFilter = medicalTermsList.filter {
+            termModel -> termModel.term.lowercase().contains(term.lowercase())
+        }
+        setVisibilityComponent(listFilter.isEmpty())
+        adapter.updateList(listFilter)
+    }
+
+    private fun setVisibilityComponent(visibility: Boolean) {
+        binding.llEmpty.isVisible = visibility
+        binding.rvMedicalTerms.isVisible = !visibility
     }
 }
--- app/src/main/java/com/carlosmonjaraz/devmon/medicalterms/TermsAdapter.kt
+++ app/src/main/java/com/carlosmonjaraz/devmon/medicalterms/TermsAdapter.kt
@@ -4,16 +4,21 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TermsAdapter(val listTerms: List<TermsModel>) : RecyclerView.Adapter<TermsHolder>() {
+class TermsAdapter(var medicalTermsList: List<TermsModel>) : RecyclerView.Adapter<TermsHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TermsHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         return TermsHolder(layoutInflater.inflate(R.layout.item_terms, parent, false))
     }
 
     override fun onBindViewHolder(holder: TermsHolder, position: Int) {
-        val item = listTerms[position]
+        val item = medicalTermsList[position]
         holder.render(item)
     }
 
-    override fun getItemCount() = listTerms.size
+    override fun getItemCount() = medicalTermsList.size
+
+    fun updateList(listTerms: List<TermsModel>) {
+        this.medicalTermsList = listTerms
+        notifyDataSetChanged()
+    }
 }
